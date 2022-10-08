dcc704ffd0a650dcbfcf65f6fa3c7f573a757cf2
mkonopacka <67000987+mkonopacka@users.noreply.github.com>
2022-10-08 10:24:13 +0200

    Add plots for sigmoid and relu

--- functions.py
+++ functions.py
@@ -1,37 +1,65 @@
 '''Numpy implementation of some cost functions, activation functions, their derivatives and other.'''
-
 import numpy as np
+from Function import Function, Quadratic
+import matplotlib.pyplot as plt
 
-def sigmoid(X: np.ndarray) -> np.ndarray:
-    return 1/(1+np.exp(-X))
+class Sigmoid(Function):
+    def __init__(self):
+        pass
 
-def sigmoid_derivative(X: np.ndarray) -> np.ndarray:
-    return sigmoid(X)*(1-sigmoid(X))
+    def __call__(self, X):
+        return 1/(1+np.exp(-X))
 
-def relu(X: np.ndarray) -> np.ndarray:
-    return np.maximum(0,X)
+    def get_derivative(self, X):
+        return self(X) * (1 - self(X))
 
-def relu_derivative(X: np.ndarray) -> np.ndarray:
-    dX = np.full_like(X, 1)
-    dX[X < 0] = 0
-    return dX
+class Relu(Function):
+    def __init__(self):
+        pass
 
-def cross_entropy(Y_: np.ndarray, Y: np.ndarray) -> np.ndarray:
-    """Computes the cross-entropy cost between Y_ and Y vectors. Returns 0-dimensional np.ndarray (shape == ()).
-    
-    Args:
-        Y_: array of size (k,m)
-        Y:  array of size (k,m)
-    """
-    # TODO What if k != 1?
-    if Y_.shape != Y.shape:
-        raise ValueError(f"Shapes of Y_ {Y_.shape} and Y {Y.shape} should be the same.")
-    if Y.shape[0] != 1:
-        raise NotImplementedError(f"Not implemented for k!=1.")
-    logprobs = np.multiply(Y, np.log(Y_)) + np.multiply((1-Y), np.log(1-Y_))
-    J = -np.mean(logprobs)
-    return np.squeeze(J)
+    def __call__(self, X):
+        return np.maximum(0,X)
+
+    def get_derivative(self, X: np.ndarray) -> np.ndarray:
+        dX = np.full_like(X, 1)
+        dX[X < 0] = 0
+        return dX
+
+class CrossEntropy(Function):
+    def __init__(self):
+        pass
+
+    def __call__(self, Y_, Y):
+        logprobs = np.multiply(Y, np.log(Y_)) + np.multiply((1-Y), np.log(1-Y_))
+        J = -np.mean(logprobs)
+        return np.squeeze(J)
+
+    def get_derivative(self, Y_, Y):
+        raise NotImplementedError
 
 if __name__ == "__main__":
-    pass 
-    #TODO test functions
+    sigmoid = Sigmoid()
+    relu = Relu()
+    print(sigmoid(1))
+    print(sigmoid.get_derivative(1))
+    xs = np.array(list(range(-100,100)))/10
+    fig, axes = plt.subplots(2,2)
+
+    axes[0,0].plot(xs,sigmoid(xs))
+    axes[0,0].grid()
+    axes[0,0].set_title("Sigmoid")
+
+    axes[0,1].plot(xs,relu(xs))
+    axes[0,1].grid()
+    axes[0,1].set_title("Relu")
+
+    axes[1,0].plot(xs, sigmoid.get_derivative(xs))
+    axes[1,0].grid()
+    axes[1,0].set_title("Sigmoid derivative")
+
+    axes[1,1].plot(xs, relu.get_derivative(xs))
+    axes[1,1].grid()
+    axes[1,1].set_title("Relu derivative")
+
+    plt.tight_layout()
+    plt.show()
